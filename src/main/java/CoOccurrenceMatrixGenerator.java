e53e5aeb1abae943af474ea2a135488e03d4fb18
Jiayao Wang <jiayao.wang@vanderbilt.edu>
2018-08-07 09:43:30 +0800

    Add files via upload

---
+++ src/main/java/CoOccurrenceMatrixGenerator.java
@@ -0,0 +1,77 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import java.io.IOException;
+
+public class CoOccurrenceMatrixGenerator {
+	public static class MatrixGeneratorMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+		// map method
+		@Override
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			//value = userid \t movie1: rating, movie2: rating...
+			String line = value.toString().trim();
+			String[] user_movie_ratings = line.split("\t");
+			if (user_movie_ratings.length != 2) {
+				System.out.println("Input format incorrect");
+				return;
+			}
+
+			String[] movie_ratings = user_movie_ratings[1].split(",");
+
+			for (int i = 0; i < movie_ratings.length; ++i) {
+				String movieA = movie_ratings[i].trim().split(":")[0];
+				for (int j = 0; j < movie_ratings.length; ++j) {
+					String movieB = movie_ratings[j].trim().split(":")[0];
+					String outputKey = movieA + ":" + movieB;
+					context.write(new Text(outputKey), new IntWritable(1));
+				}
+			}
+			
+		}
+	}
+
+	public static class MatrixGeneratorReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+		// reduce method
+		@Override
+		public void reduce(Text key, Iterable<IntWritable> values, Context context)
+				throws IOException, InterruptedException {
+			//calculate each two movies have been watched by how many people
+			int sum = 0;
+			for (IntWritable value:values) {
+				sum += value.get();
+			}
+			context.write(new Text(key), new IntWritable(sum));
+		}
+	}
+	
+	public static void main(String[] args) throws Exception{
+		
+		Configuration conf = new Configuration();
+		
+		Job job = Job.getInstance(conf);
+		job.setMapperClass(MatrixGeneratorMapper.class);
+		job.setReducerClass(MatrixGeneratorReducer.class);
+		
+		job.setJarByClass(CoOccurrenceMatrixGenerator.class);
+		
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		
+		TextInputFormat.setInputPaths(job, new Path(args[0]));
+		TextOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		job.waitForCompletion(true);
+		
+	}
+}
